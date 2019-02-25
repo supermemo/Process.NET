6977b6b365e9136d9f20c4756d7661424cc581cd
Alexis <alexis@incogito.org>
2019-02-25 21:26:13 +0100

    - Automatic version

--- src/Process.NET/Properties/AssemblyInfo.cs
+++ src/Process.NET/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -37,3 +37,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
